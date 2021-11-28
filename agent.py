b6219da3f7925437414466d18407f7fbb13569b3
2471389876 <2471389876@qq.com>
2021-11-28 14:18:28 +0800

    init

--- agent.py
+++ agent.py
@@ -6,7 +6,7 @@ import random
 class Agent:
     def __init__(self):
         # 状态集合包括 ： 距离，数据量，当前时隙
-        self.State = np.array([1, 2000, 0])
+        self.State = self.reset()
 
     def reset(self):
-        self.State = np.array([1, 2000, 0])
+        return np.array([1, 2000, 0])
